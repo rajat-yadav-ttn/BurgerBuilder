ef2cd49c34cfe5fd10330422d43658676986962e
rajat-yadav-ttn <rajat.yadav@tothenew.com>
2020-03-30 21:29:42 +0530

    .

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount(){
+        console.log(this.props);
         axios.get('https://burgerbuilder-ff179.firebaseio.com/ingredients.json')
             .then(response=>{
                 this.setState({ingredients:response.data})
